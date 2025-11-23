9af025f250391181d04f67f57be742e474f7a214
AndriiPetruk <2002ANRIK@gmail.com>
2025-11-23 13:57:07 +0100

    add claims logging

--- Starter files/ImageGallery.Client/Controllers/GalleryController.cs
+++ Starter files/ImageGallery.Client/Controllers/GalleryController.cs
@@ -1,10 +1,15 @@
 ﻿using ImageGallery.Client.ViewModels;
 using ImageGallery.Model;
-using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Text;
 using System.Text.Json;
 
 namespace ImageGallery.Client.Controllers
 {
+    [Authorize]
     public class GalleryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -20,6 +25,8 @@ namespace ImageGallery.Client.Controllers
 
         public async Task<IActionResult> Index()
         {
+            await LogIdentityInformation();
+
             var httpClient = _httpClientFactory.CreateClient("APIClient");
 
             var request = new HttpRequestMessage(
@@ -174,6 +181,23 @@ namespace ImageGallery.Client.Controllers
             response.EnsureSuccessStatusCode();
 
             return RedirectToAction("Index");
+        }
+
+        public async Task LogIdentityInformation()
+        {
+            var identityToken = await HttpContext
+                .GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+
+            var userClaimStringBuilder = new StringBuilder();
+
+            foreach(var claim in User.Claims)
+            {
+                userClaimStringBuilder.AppendLine(
+                    $"Claim type: {claim.GetType} - Claim value: {claim}");
+            }
+
+            _logger.LogInformation($"Identity token & user lcaims: " +
+                $"{identityToken} \n{userClaimStringBuilder}");
         }
     }
 }
