e9b8a0b021756ca79cbca88d09d84f739c4676b1
AndriiPetruk <2002ANRIK@gmail.com>
2025-11-23 13:31:03 +0100

    add configuration for ids and ids

---
+++ Marvin.IDP/Pages/TestUsers.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using IdentityModel;
+using System.Security.Claims;
+using System.Text.Json;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Test;
+
+namespace Marvin.IDP;
+
+public static class TestUsers
+{
+    public static List<TestUser> Users
+    {
+        get
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "3fcc057b-e1f6-456f-9f14-85a466b6eb63",
+                    Username = "David",
+                    Password = "password",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "David"),
+                        new Claim(JwtClaimTypes.FamilyName, "Flagg"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "d21a6949-5b2b-4ef3-a424-a8cbfbaaf1b6",
+                    Username = "Emma",
+                    Password = "password",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "Emma"),
+                        new Claim(JwtClaimTypes.FamilyName, "Flagg")
+                    }
+                }
+            };
+        }
+    }
+}
